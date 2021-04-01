50b8daf7dd4f1e35865625bf34e28dc696054bf4
MubinaKamberovic <mubinakamberovic9@gmail.com>
2021-04-01 09:42:59 +0200

    Kreirana osnovna konfiguracija

---
+++ src/main/java/ba/unsa/etf/nwt/easytravel/EasytravelApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.easytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasytravelApplication.class, args);
+    }
+
+}
